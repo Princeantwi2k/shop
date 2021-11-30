df587afd2b6f34d871346ee8c542a312ca66da39
princeantwi2k <antwi2k@gmail.com>
2021-11-30 20:50:16 +0800

    addeed Homepage

---
+++ Src/page/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, StyleSheet,FlatList,Image,Text} from 'react-native';
+import Buttomnavigator from '../Route/ButtomNavigator';
+
+const Homepage = () => {
+    return     <Buttomnavigator  />
+    
+     
+}
+
+
+   
+
+
+export default Homepage;

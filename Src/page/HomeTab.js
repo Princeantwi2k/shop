2cbd1ae4ec91c1a809c534c4041f04040296a2fa
princeantwi2k <antwi2k@gmail.com>
2021-11-30 20:50:35 +0800

    added HomeTab

---
+++ Src/page/HomeTab.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet,Text} from 'react-native';
+
+const Hometab = () => {
+    return (
+        <View>
+            <Text>the man</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Hometab;
